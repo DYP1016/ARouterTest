1f340cdb72aada84ba969f76f7605ba62915318f
DYP <526642111@qq.com>
2021-04-19 16:58:08 +0800

    Initial commit

---
+++ module_account/src/main/java/com/dyp/ui/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.dyp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dyp.test.module_account.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
---
+++ module_main/src/main/java/com/dyp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dyp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dyp.test.module_main.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
